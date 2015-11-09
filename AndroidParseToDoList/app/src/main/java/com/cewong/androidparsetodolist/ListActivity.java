f8056ba37b737d93f1b730bbb1422396b55dca76
CEWong <c0wong01@louisville.edu>
2015-11-09 15:35:32 -0500

    End Part 2. Attempt to debug

--- AndroidParseToDoList/app/src/main/java/com/cewong/androidparsetodolist/ListActivity.java
+++ AndroidParseToDoList/app/src/main/java/com/cewong/androidparsetodolist/ListActivity.java
@@ -1,15 +1,31 @@
 package com.cewong.androidparsetodolist;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class ListActivity extends ActionBarActivity {
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import net.dkpat.todolist.model.Task;
+import net.dkpat.todolist.model.TasksAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListActivity extends AppCompatActivity {
 
     private Button btnCreate;
-    private Button btnUpdate;
+    private TasksAdapter adapter;
+    private ArrayList<Task> arrayOfTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +43,41 @@ public class ListActivity extends ActionBarActivity {
             }
         });
 
-        btnUpdate = (Button) findViewById(R.id.btnUpdate);
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
+        arrayOfTasks = new ArrayList<Task>();
+        adapter = new TasksAdapter(this, arrayOfTasks);
+        ListView listView = (ListView) findViewById(R.id.lv_task);
+        listView.setAdapter(adapter);
 
-            public void onClick(View arg0) {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Task");
 
-                Intent updateActivity = new Intent(getApplicationContext(), UpdateActivity.class);
-                startActivity(updateActivity);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> taskList, ParseException e) {
+                if (e == null) {
+                    for (ParseObject tasks : taskList) {
 
+                        Task task = new Task();
+
+                        task.setTaskId(tasks.getObjectId());
+                        task.setName(tasks.getString("name"));
+                        task.setDescription(tasks.getString("description"));
+
+                        Log.d("name", task.getName());
+                        adapter.add(task);
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Task task = adapter.getItem(position);
+
+                Intent intent = new Intent(getBaseContext(), UpdateActivity.class);
+                intent.putExtra("objectId", task.getTaskId());
+                startActivity(intent);
             }
         });
     }
